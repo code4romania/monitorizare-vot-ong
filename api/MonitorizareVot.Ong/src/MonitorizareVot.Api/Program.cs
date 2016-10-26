f773378c8ed3b8ce76b00db99ecc439bc459a8c2
dragos.hont <dragos.hont@gmail.com>
2016-10-26 20:56:38 +0300

    adaugare monitorizare vot ong domain

--- api/MonitorizareVot.Ong/src/MonitorizareVot.Api/Program.cs
+++ api/MonitorizareVot.Ong/src/MonitorizareVot.Api/Program.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
 
-namespace MonitorizareVot.Api
+namespace MonitorizareVot.Ong.Api
 {
     public class Program
     {
---
+++ api/MonitorizareVot.Ong/src/MonitorizareVot.Domain.Ong/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitorizareVot.Domain.Ong
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
