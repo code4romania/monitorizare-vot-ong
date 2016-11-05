c3825a85ee897ab6146a8836582dba2a143327b8
Lidia <necula_lidia@yahoo.com>
2016-11-05 19:45:52 +0200

    #1 Add more integration tests for the api/v1/raspunsuri endpoint

--- api/MonitorizareVot.Ong/test/MonitorizareVot.Ong.Api.Tests/Controllers/RaspunsuriTests.cs
+++ api/MonitorizareVot.Ong/test/MonitorizareVot.Ong.Api.Tests/Controllers/RaspunsuriTests.cs
@@ -1,7 +1,10 @@
-﻿using MonitorizareVot.Ong.Api.Common;
+﻿using Microsoft.EntityFrameworkCore;
+using MonitorizareVot.Domain.Ong.Models;
+using MonitorizareVot.Ong.Api.Common;
 using MonitorizareVot.Ong.Api.Extensions;
 using MonitorizareVot.Ong.Api.ViewModels;
 using Newtonsoft.Json;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using Xunit;
@@ -55,13 +58,27 @@ namespace MonitorizareVot.Ong.Api.Tests.Controllers
         }
 
         [Theory]
-        [InlineData(1, 50, true, 0)]
-        [InlineData(2, 50, false, 9)]
-        [InlineData(1, 5, false, 9)]
-        [InlineData(1, 50, false, 9)]
-        public async void GetRaspunsuri_GetSectiiCuObservatori_ReturneazaRaspunsPaginat(int page, int pageSize, bool urgent, int expectedTotal)
+        [InlineData(1, 50, true)]
+        [InlineData(2, 50, false)]
+        [InlineData(1, 5, false)]
+        [InlineData(1, 50, false)]
+        public async void GetRaspunsuri_GetSectiiCuObservatori_ReturneazaRaspunsPaginat(int page, int pageSize, bool urgent)
         {
             // ARRANGE
+            const int idONG = 1; // TODO set the idONG in token and use it as a param for the test
+            int countObservatori;
+
+            using (var context = new OngContext(_raspunsuriFixture.ContextOptions))
+            {
+                countObservatori = await context.Raspuns
+                 .Where(x => x.IdObservatorNavigation.IdOng == idONG && x.IdRaspunsDisponibilNavigation.RaspunsCuFlag == urgent)
+                 .Select(y =>
+                   new { y.IdObservator, Observator = y.IdObservatorNavigation.NumeIntreg, IdSectie = y.IdSectieDeVotare, Sectie = y.IdSectieDeVotareNavigation.DenumireUat, y.DataUltimeiModificari }
+                  )
+                 .Distinct()
+                 .CountAsync();
+            }
+
             // ACT
             var response = await _client.GetAsync($"api/v1/raspunsuri?page={page}&pagesize={pageSize}&urgent={urgent}");
 
@@ -74,7 +91,53 @@ namespace MonitorizareVot.Ong.Api.Tests.Controllers
             Assert.True(pageSize >= model.Data.Raspunsuri.Count);
             Assert.Equal(page, model.Data.Page);
             Assert.Equal(pageSize, model.Data.PageSize);
-            Assert.Equal(expectedTotal, model.Data.Total);
+            Assert.Equal(countObservatori, model.Data.Total);
+        }
+
+        [Theory]
+        [InlineData(2, 5, false)]
+        [InlineData(1, 50, false)]
+        [InlineData(2, 10, true)]
+        [InlineData(1, 10, true)]
+        public async void GetRaspunsuri_GetSectiiCuObservatori_ReturneazaRaspunsuriOrdonateDupaDataModificareDesc(int page, int pageSize, bool urgent)
+        {
+            // ARRANGE
+            const int idONG = 1; // TODO set the idONG in token and use it as a param for the test
+            RaspunsModel first, last;
+
+            using (var context = new OngContext(_raspunsuriFixture.ContextOptions))
+            {
+                var sectiiCuObservatori = await context.Raspuns
+                  .Where(x => x.IdObservatorNavigation.IdOng == idONG && x.IdRaspunsDisponibilNavigation.RaspunsCuFlag == urgent)
+                  .Select(y =>
+                    new { y.IdObservator, Observator = y.IdObservatorNavigation.NumeIntreg, IdSectie = y.IdSectieDeVotare, Sectie = y.IdSectieDeVotareNavigation.DenumireUat, y.DataUltimeiModificari }
+                   )
+                  .Distinct()
+                  .OrderByDescending(x => x.DataUltimeiModificari)
+                  .Skip((page - 1) * pageSize)
+                  .Take(pageSize)
+                  .ToListAsync();
+                var sectieFirst = sectiiCuObservatori.First();
+                var sectieLast = sectiiCuObservatori.Last();
+                first = new RaspunsModel { IdObservator = sectieFirst.IdObservator, IdSectie = sectieFirst.IdSectie };
+                last = new RaspunsModel { IdObservator = sectieFirst.IdObservator, IdSectie = sectieLast.IdSectie };
+            }
+
+            // ACT
+            var response = await _client.GetAsync($"api/v1/raspunsuri?page={page}&pagesize={pageSize}&urgent={urgent}");
+
+            // ASSERT
+            response.EnsureSuccessStatusCode();
+            var model = JsonConvert.DeserializeObject<Raspuns<ListaRaspunsuri<RaspunsModel>>>(await response.Content.ReadAsStringAsync());
+            Assert.NotNull(model);
+            Assert.NotNull(model.Data);
+            Assert.NotNull(model.Data.Raspunsuri);
+            var firstModel = model.Data.Raspunsuri.First();
+            var lastModel = model.Data.Raspunsuri.Last();
+            Assert.Equal(first.IdObservator, firstModel.IdObservator);
+            Assert.Equal(first.IdSectie, firstModel.IdSectie);
+            Assert.Equal(last.IdObservator, firstModel.IdObservator);
+            Assert.Equal(last.IdObservator, firstModel.IdObservator);
         }
 
         [Fact(Skip = "Actualizat testul atunci cand IdOng este luat din token")]
