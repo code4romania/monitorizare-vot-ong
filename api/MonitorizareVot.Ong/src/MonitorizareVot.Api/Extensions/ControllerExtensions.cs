1c620f4bd901ef783c5233e9186c4b1a5c0e3d2e
dragos.hont <dragos.hont@gmail.com>
2016-12-06 00:54:59 +0200

    scos mock data pe lista de raspunsuri

---
+++ api/MonitorizareVot.Ong/src/MonitorizareVot.Api/Extensions/ControllerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MonitorizareVot.Ong.Api.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static int GetIdOngOrDefault(this Controller controller, int defaultIdOng)
+        {
+            int result;
+            return int.TryParse(controller.User.Claims.FirstOrDefault(a => a.Type == "IdOng")?.Value, out result) 
+                ? result 
+                : defaultIdOng;
+        }
+    }
+}
