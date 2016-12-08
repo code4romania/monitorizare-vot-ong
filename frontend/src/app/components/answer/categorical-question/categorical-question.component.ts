ce061e527af5251984a0900c11d64519d4977494
Stoicescu Cristi <cristian.stoicescu@komunomo.com>
2016-12-09 01:41:56 +0200

    Added question hightling and validation

--- frontend/src/app/components/answer/categorical-question/categorical-question.component.ts
+++ frontend/src/app/components/answer/categorical-question/categorical-question.component.ts
@@ -1,3 +1,4 @@
+import { environment } from '../../../../environments/environment';
 import { Note } from '../../../models/note.model';
 import { BaseAnswer } from '../../../models/base.answer.model';
 import { CompletedAnswer } from '../../../models/completed.answer.model';
@@ -17,16 +18,24 @@ export class CategoricalQuestionComponent implements OnInit {
 
   @Input('completedAnswers') set inputCompletedAnswers(value: CompletedAnswer[]) {
     if (value && value.length) {
-      this.completedAnswers = _.keyBy(value, value => value.idOptiune);
+      if(this.isSingle && value && value.length > 1 && !environment.production){
+        try{
+          console.log(`Multiple answers on question with id ${this.question.idIntrebare}`)
+        } catch(ex){}
+      }
+      this.completedAnswers = _.keyBy(value, value => value.idOptiune)
     } else {
       this.completedAnswers = undefined;
     }
-  }
-
-  completedAnswers: _.Dictionary<CompletedAnswer>;
+  }  
+  completedAnswers: _.Dictionary<CompletedAnswer> = {};
 
   @Input()
   note: Note
+
+  get isFlagged(){
+    return _.some(_.values(this.completedAnswers),a => a.raspunsCuFlag);
+  }
 
   get isTextQuestion() {
     return this.question.idTipIntrebare === 2 || this.question.idTipIntrebare === 3
@@ -34,12 +43,18 @@ export class CategoricalQuestionComponent implements OnInit {
   get isSingle() {
     return this.question.idTipIntrebare === 0 || this.question.idTipIntrebare === 4;
   }
+
   isChecked(answer: BaseAnswer) {
     return this.completedAnswers && this.completedAnswers[answer.idOptiune];
   }
   isTextAnswer(answer: BaseAnswer) {
     return this.isTextQuestion && answer.seIntroduceText;
   }
+
+  isFlaggedAnswer(answer: BaseAnswer){
+    return _.some(_.values(this.completedAnswers),a => a.raspunsCuFlag && a.idOptiune === answer.idOptiune);
+  }
+
   answerTextValue(answer: BaseAnswer) {
     return this.completedAnswers && this.completedAnswers[answer.idOptiune] && this.completedAnswers[answer.idOptiune].value;
   }
