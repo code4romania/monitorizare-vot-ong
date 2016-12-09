8661f0b6e7ac334db75cde5306d3d1ba748a3158
Stoicescu Cristi <cristian.stoicescu@komunomo.com>
2016-12-09 04:11:20 +0200

    Fixed edge case pagination bug

--- frontend/src/app/shared/pagination.service.ts
+++ frontend/src/app/shared/pagination.service.ts
@@ -2,6 +2,12 @@ export function shouldLoadPage(page: number, pageSize: number, arrayLen) {
     if (page === undefined || pageSize === undefined) {
         return true;
     }
-
-    return page * pageSize > arrayLen;
+    if(page * pageSize > arrayLen){
+        if((page -1) * pageSize < arrayLen){
+            return false
+        }
+        return true;
+    } else {
+        return false;
+    }
 }
