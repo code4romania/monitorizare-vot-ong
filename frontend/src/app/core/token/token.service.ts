932271556d1afa7fd427d3b3e85bd4e5bf309e22
Stoicescu Cristi <cristian.stoicescu@komunomo.com>
2016-12-09 03:12:47 +0200

    Fixed forms bug when not logged in

--- frontend/src/app/core/token/token.service.ts
+++ frontend/src/app/core/token/token.service.ts
@@ -1,3 +1,4 @@
+import { Observable, Observer } from 'rxjs/Rx';
 import { locale } from 'moment';
 import { Injectable } from '@angular/core';
 import { JwtHelper } from 'angular2-jwt';
@@ -5,18 +6,33 @@ import { JwtHelper } from 'angular2-jwt';
 @Injectable()
 export class TokenService {
   public tokenKey: string = 'token-id';
-  private _token:string = undefined
+  private _token: string = undefined
 
   private jwtHelper = new JwtHelper();
   private _isRefreshing: boolean;
+
+  tokenStream: Observable<string>;
+  observers: Observer<string>[] = [];
+
 
   public get isRefreshing() {
     return this._isRefreshing;
   }
 
   constructor() {
-    this._token = localStorage.getItem(this.tokenKey);
-   }
+    this._token = localStorage.getItem(this.tokenKey)
+    this.setTokenStream();
+  }
+
+  setTokenStream() {
+    this.tokenStream = Observable.create((obs:Observer<string>) => {
+      obs.next(this._token);
+      this.observers.push(obs);
+      return () => {
+        this.observers = this.observers.filter(stateObserver => stateObserver === obs);
+      }
+    });
+  }
 
   public get token() {
     return this._token;
@@ -24,6 +40,7 @@ export class TokenService {
   public set token(value) {
     this._token = value;
     localStorage.setItem(this.tokenKey, value);
+    this.observers.forEach(obs => obs.next(value));
   }
   public isTokenExpired() {
     var token = this.token;
