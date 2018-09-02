ba7e6de6acf4659167e3107bba55cf08f4e38093
Razvan Ciuca <razvann88@gmail.com>
2018-09-02 15:54:05 +0300

    add ngx-translate

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import 'rxjs/Rx';
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang('ro');
+    translate.use('ro');
+  }
 }
