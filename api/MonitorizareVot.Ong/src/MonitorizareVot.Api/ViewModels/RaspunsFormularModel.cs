8361113f04639c6b675e8f6f2b68639420804170
Bogdan <bogdan.vizureanu@gmail.com>
2018-10-21 13:23:13 +0300

    `RaspunsFormular` mappings

--- api/MonitorizareVot.Ong/src/MonitorizareVot.Api/ViewModels/RaspunsFormularModel.cs
+++ api/MonitorizareVot.Ong/src/MonitorizareVot.Api/ViewModels/RaspunsFormularModel.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using System;
 using System.Collections.Generic;
+using AutoMapper;
+using MonitorizareVot.Domain.Ong.Models;
 
 namespace MonitorizareVot.Ong.Api.ViewModels
 {
@@ -11,6 +13,20 @@ namespace MonitorizareVot.Ong.Api.ViewModels
         public DateTime? OraPlecarii { get; set; }
         public DateTime? OraSosirii { get; set; }
         public bool? PresedinteBesvesteFemeie { get; set; }
+    }
+
+    public class RaspunsFomularProfile : Profile
+    {
+        public RaspunsFomularProfile()
+        {
+            CreateMap<PollingStationInfo, RaspunsFormularModel>()
+                .ForMember(dest => dest.DataUltimeiModificari, o => o.MapFrom(src => src.LastModified))
+                .ForMember(dest => dest.EsteZonaUrbana, o => o.MapFrom(src => src.UrbanArea))
+                .ForMember(dest => dest.OraPlecarii, o => o.MapFrom(src => src.ObserverLeaveTime))
+                .ForMember(dest => dest.OraSosirii, o => o.MapFrom(src => src.ObserverArrivalTime))
+                .ForMember(dest => dest.PresedinteBesvesteFemeie, o => o.MapFrom(src => src.IsPollingStationPresidentFemale))
+                ;
+        }
     }
 
     public class RaspunsuriFormularQuery : IRequest<RaspunsFormularModel>
