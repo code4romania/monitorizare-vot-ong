c4b0158c141fcb929615ef3cca4e3d731c12a87e
Andy Radulescu <eduard-andrei.radulescu@bawagpsk.com>
2019-10-20 13:54:46 +0200

    fix some styling issues

--- frontend2/src/app/app.module.ts
+++ frontend2/src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
+import { CoreModule } from './core/core.module';
+import { AppStoreModule } from './store/store.module';
+import { SharedModule } from './shared/shared.module';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { AnswersService } from './services/answers.service';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './routing/app.routing.module';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CoreModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    AppStoreModule,
+    ComponentsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [AnswersService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
