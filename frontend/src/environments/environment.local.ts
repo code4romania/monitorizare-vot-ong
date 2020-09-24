ac28f84bdfe9e0b7417815d289950215b92829bf
Irina Borozan <anirib@gmail.com>
2020-09-25 00:28:40 +0300

    updated api and observer manual links

--- frontend/src/environments/environment.local.ts
+++ frontend/src/environments/environment.local.ts
@@ -2,6 +2,6 @@ import { EnvironmentConfig } from 'typings';
 
 export const environment: EnvironmentConfig = {
   production: false,
-  observerGuideUrl: 'http://qa-monitorizare-vot-ghid.azurewebsites.net/',
-  apiUrl: 'http://localhost:5000'
+  observerGuideUrl: 'https://votcorect.ro/wp-content/uploads/2020/09/Manualul-observatorului-locale-2020-public.pdf',
+  apiUrl: 'https://localhost:5001'
 };
