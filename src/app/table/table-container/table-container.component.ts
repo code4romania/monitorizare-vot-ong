790f28e2d5222e2e78d1ab626d62f3a341287979
Andrei Gatej <andreigtj01@gmail.com>
2020-10-16 23:15:07 +0300

    feat(table-container): add `selected-zone`'s feature
    * dynamically showing the number of selected
    * perform certain actions(e.g delete, notification) with the selected rows

--- src/app/table/table-container/table-container.component.ts
+++ src/app/table/table-container/table-container.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ContentChild, ContentChildren, Inject, Input, OnInit, TemplateRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ContentChild, ContentChildren, EventEmitter, Inject, InjectionToken, Input, OnInit, Output, TemplateRef } from '@angular/core';
 import { BASE_BUTTON_VARIANTS, Variants } from 'src/app/shared/base-button/base-button.component';
 
 export interface TableColumn {
@@ -6,6 +6,16 @@ export interface TableColumn {
   canBeSorted?: boolean;
   propertyName?: string;
 }
+
+export enum SelectedZoneEvents {
+  DELETE,
+  NOTIFCATION
+}
+
+const SELECTED_ZONE_EVENTS = new InjectionToken('SELECTED_ZONE_EVENTS', {
+  providedIn: 'root',
+  factory: () => SelectedZoneEvents,
+});
 
 @Component({
   selector: 'app-table-container[columns]',
@@ -18,13 +28,18 @@ export class TableContainerComponent implements OnInit {
   @Input() rows: unknown[] = [];
   @Input() idKey = 'id';
 
-  @ContentChild(TemplateRef) tdContent: TemplateRef<any>;
+  @Output() selectedZoneEvent = new EventEmitter();
 
+  @ContentChild(TemplateRef) tdContent: TemplateRef<any>;
+  
   selectedRows: { [rowId: string]: boolean } = {};
   allSelected = false;
   nrSelectedRows = 0;
 
-  constructor(@Inject(BASE_BUTTON_VARIANTS) public BaseButtonVariants: typeof Variants,) { }
+  constructor(
+    @Inject(BASE_BUTTON_VARIANTS) public BaseButtonVariants: typeof Variants,
+    @Inject(SELECTED_ZONE_EVENTS) public Events: typeof SelectedZoneEvents
+  ) { }
 
   ngOnInit(): void {
   }
@@ -35,18 +50,30 @@ export class TableContainerComponent implements OnInit {
       this.selectedRows = {};
       this.nrSelectedRows = this.rows.length;
     } else {
-      this.allSelected = false;
-      this.nrSelectedRows = 0;
+      this.clearSelectedRows();
     }
   }
 
   toggleRow (rowId) {
-    this.selectedRows[rowId] = this.allSelected ? false : !this.selectedRows[rowId];
+    this.selectedRows[rowId] = typeof this.selectedRows[rowId] === 'boolean' 
+      ? !this.selectedRows[rowId] 
+      : this.allSelected ? false : true; 
 
     this.nrSelectedRows += (this.selectedRows[rowId] ? 1 : -1);
 
     if (!this.nrSelectedRows) {
       this.allSelected = false;
     }
+  }
+
+  sendSelectedZoneEvent (ev: SelectedZoneEvents) {
+    this.selectedZoneEvent.emit(ev);
+    this.clearSelectedRows();
+  }
+
+  private clearSelectedRows () {
+    this.allSelected = false;
+    this.nrSelectedRows = 0;
+    this.selectedRows = {};
   }
 } 
