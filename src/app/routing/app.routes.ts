0f8eb5f7b1c04c741bc6656242015688be0e4511
Andrei Gatej <andreigtj01@gmail.com>
2020-11-19 11:37:09 +0200

    feat(answers): add `CanLoad` guard to `answers` module

--- src/app/routing/app.routes.ts
+++ src/app/routing/app.routes.ts
@@ -27,21 +27,8 @@ export let appRoutes: Routes = [
   },
   {
     path: 'answers',
-    // component: AnswerComponent,
-    // canActivate: [AuthGuard, AnswerListGuard],
-    // children: [
-    //   {
-    //     path: 'details/:idObserver/:idPollingStation',
-    //     component: AnswerDetailsComponent,
-    //     canActivate: [AuthGuard, AnswerDetailsGuard],
-    //   },
-    //   {
-    //     path: '',
-    //     canActivate: [AuthGuard],
-    //     component: AnswerDetailsComponent,
-    //   },
-    // ],
-    loadChildren: () => import('src/app/answers/answers.module').then(m => m.AnswersModule)
+    loadChildren: () => import('src/app/answers/answers.module').then(m => m.AnswersModule),
+    canLoad: [AuthGuard],
   },
   {
     path: 'urgents',
