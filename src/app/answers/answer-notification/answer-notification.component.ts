a8370c2a670623f453dd6a202aaf87699df4991e
Andrei Gatej <andreigtj01@gmail.com>
2020-11-23 21:32:57 +0200

    feat(answers-notification): send notification

--- src/app/answers/answer-notification/answer-notification.component.ts
+++ src/app/answers/answer-notification/answer-notification.component.ts
@@ -1,7 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { pluck, tap } from 'rxjs/operators';
+import { concat, Observable, of, timer } from 'rxjs';
+import { catchError, mapTo, pluck, tap } from 'rxjs/operators';
+import { NotificationsService } from 'src/app/services/notifications.service';
 import { BASE_BUTTON_VARIANTS, Variants } from 'src/app/shared/base-button/base-button.component';
 import { getSpecificThreadByObserver } from 'src/app/store/answer/answer.selectors';
 import { AppState } from 'src/app/store/store.module';
@@ -17,7 +19,8 @@ export class AnswerNotificationComponent {
       tap(v => v === void 0 && this.router.navigate(['/answers'], { relativeTo: this.route })),
       pluck('observerName'),
     );
-  
+  actionResultMessage$: Observable<string> = of('');
+
   buttonStyles = {
     fontWeight: 'normal',
     padding: '0.6rem 1rem',
@@ -29,10 +32,23 @@ export class AnswerNotificationComponent {
     private router: Router,
     private route: ActivatedRoute,
     private store: Store<AppState>,
+    private notificationService: NotificationsService,
     @Inject(BASE_BUTTON_VARIANTS) public BaseButtonVariants: typeof Variants
   ) { }
 
-  onSubmit (values) {
-    console.log({ values })
+  onSubmit (values: { title: string, message: string }) {
+    this.actionResultMessage$ = this.notificationService.pushNotification({
+      ...values,
+      channel: 'Firebase',
+      from: 'Monitorizare Vot',
+      recipients: [this.route.snapshot.params.idObserver]
+    }).pipe(
+      catchError(err => of(err.message)),  
+    );
+
+    this.actionResultMessage$ = concat(
+      this.actionResultMessage$,
+      timer(5000).pipe(mapTo('')),
+    );
   }
 }
