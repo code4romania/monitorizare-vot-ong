b368a23f50ea8ac4994242e6e0a8ee7d2f0a8b8c
Andrei Gatej <andreigtj01@gmail.com>
2020-11-26 13:47:24 +0200

    feat(answer-questions): show note in modal when the `View Note` is called

--- src/app/answers/answer-questions/answer-questions.component.ts
+++ src/app/answers/answer-questions/answer-questions.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, AfterViewInit, ChangeDetectionStrategy, Component, ContentChildren, ElementRef, Inject, Input, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { AfterContentInit, AfterViewInit, ChangeDetectionStrategy, Component, ContentChildren, ElementRef, EventEmitter, Inject, Input, OnInit, Output, QueryList, ViewChildren } from '@angular/core';
 import { BASE_BUTTON_VARIANTS, Variants } from 'src/app/shared/base-button/base-button.component';
 import { DisplayedNote, SectionsState } from '../answers.model';
 
@@ -12,6 +12,8 @@ export class AnswerQuestionsComponent implements AfterViewInit {
   @Input() sections = [];
   @Input() sectionsState: SectionsState;
   @Input('scrolled-question-id') scrolledQuestionId: number;
+
+  @Output() showNoteClicked = new EventEmitter();
 
   @ViewChildren('question') questionsDivs: QueryList<ElementRef>;
 
