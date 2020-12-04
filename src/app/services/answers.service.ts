c5d9546767a2046263963ab6504aa40ba0c251d8
Nicolae Stihhi <nicolae.stihhi@gmail.com>
2020-12-04 11:30:01 +0200

    321 Added export notes button (#322)

--- src/app/services/answers.service.ts
+++ src/app/services/answers.service.ts
@@ -3,6 +3,8 @@ import { Location } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { ApiService, QueryParamBuilder } from '../core/apiService/api.service';
 import { AnswerExtraConstructorData } from '../models/answer.extra.model';
+import {HttpParams} from '@angular/common/http';
+import {first} from 'rxjs/operators';
 
 @Injectable({
 	providedIn: 'root'
@@ -17,15 +19,24 @@ export class AnswersService {
 		let paramBuilder = QueryParamBuilder
       .Instance('/api/v1/export/all');
 
-  for (const key in filter) {
-		    const value = filter[key];
-		    paramBuilder = paramBuilder.withParam(key, value);
+    for (const key in filter) {
+      if (filter.hasOwnProperty(key)) {
+        const value = filter[key];
+        paramBuilder = paramBuilder.withParam(key, value);
+      }
 		}
 		const urlWithParams = paramBuilder.build();
 
 		const url: string = Location.joinWithSlash(this.baseUrl, urlWithParams);
-		return this.http.get<Blob>(url, {responseType: 'blob' as 'json'});
+		return this.http.get<Blob>(url, {responseType: 'blob' as 'json'}).pipe(first());
 	}
+
+  downloadNotes(filter: AnswersPackFilter) {
+    const filterStringEntries = Object.entries(filter).map(([k, v]) => [k, v.toString()]);
+    const params = new HttpParams({fromObject: Object.fromEntries(filterStringEntries)});
+    const url = Location.joinWithSlash(this.baseUrl, '/api/v1/export/all/notes');
+    return this.http.get<Blob>(url, {params, responseType: 'blob' as 'json'}).pipe(first());
+  }
 
 	fetchExtraDetailsForObserver (observerId: number, sectionId: number) {
 		return this.http.get<AnswerExtraConstructorData>(this.extraDetailsURL, {
