22c03910978ee379d3650855a962d25b21c47d17
Utwo <mihai.legat@gmail.com>
2021-01-08 16:22:44 +0200

    Move prod env to Vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 import { EnvironmentConfig } from '../typings';
 
 export const environment: EnvironmentConfig = {
-  production: false,
+  production: true,
   observerGuideUrl:
     'https://fiecarevot.ro/wp-content/uploads/2020/12/Manual-observatori-FV-parlamentare2020.pdf',
   apiUrl: 'https://api.votemonitor.org/',
