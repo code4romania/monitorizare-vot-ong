2957362efc9eee1531f66f79575174e1e3c0c341
Andrei Ioniță <hi@andrei.io>
2023-08-22 13:51:48 +0100

    update build

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -4,5 +4,5 @@ export const environment: EnvironmentConfig = {
   production: true,
   observerGuideUrl:
     'https://fiecarevot.ro/wp-content/uploads/2020/12/Manual-observatori-FV-parlamentare2020.pdf',
-  apiUrl: 'https://api.votemonitor.org/',
+  apiUrl: 'https://monitorizare-vot-api.staging.heroesof.tech/',
 };
